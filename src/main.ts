2e2e2c1afa13f3239232c5af64ce39b00b758ace
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2021-01-14 21:01:41 +0900

    create initial project structure

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import store from "./store";
+import "semantic-ui-css/semantic.css";
+import "@/assets/css/global.css";
 
 createApp(App)
   .use(store)
