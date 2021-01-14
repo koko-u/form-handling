6e70973237db9d7f9b1fb836076c92f9ccecfaa3
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2021-01-14 22:53:26 +0900

    Text Fields

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="ui container"></div>
+  <div class="ui container">
+    <h2 class="ui header">Fullstack Clothing Inquiry Sheet</h2>
+    <InputForm />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import InputForm from "./components/InputForm.vue";
 
 export default defineComponent({
   name: "App",
   components: {
+    InputForm
   }
 });
 </script>
---
+++ src/components/InputForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="input-form">
+    <form class="ui form" @submit.prevent="submitForm">
+      <div class="field">
+        <input type="text" v-model="newItem" placeholder="Add an item!" />
+      </div>
+      <button class="ui button">Submit</button>
+    </form>
+    <div class="ui segment" v-if="itemList.length > 0">
+      <h4 class="ui header">Items</h4>
+      <ul>
+        <li class="item" v-for="item in itemList" :key="item">{{ item }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "InputForm",
+  props: {},
+  setup(props, context) {
+    const newItem = ref<string>("");
+    const itemList = ref<string[]>([]);
+    const submitForm = (e: Event) => {
+      itemList.value.push(newItem.value);
+      newItem.value = "";
+    };
+
+    return {
+      submitForm,
+      newItem,
+      itemList
+    };
+  }
+});
+</script>
+
+<style lang="scss" scoped></style>
